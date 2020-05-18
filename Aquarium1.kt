35767394f8d5e923c97e3eed7b1fda56112ac779
Ipshita Ghosh <iuippi3@gmail.com>
2020-05-18 21:13:34 +0530

    constructor

--- Aquarium1.kt
+++ Aquarium1.kt
@@ -2,9 +2,9 @@ package Aquarium
 
 //declaring a class with constructor
 class Aquarium (length:Int, width:Int, height:Int){
-    var length: Int = 10
-    var width: Int = 70
-    var height: Int = 30
+    var length = length
+    var width = width
+    var height = height
 
 
     fun volume() =  length * height * width / 1000
