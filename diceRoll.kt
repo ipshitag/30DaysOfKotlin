5e983b071aae2187ece86d7fb194660f17ffe098
Ipshita Ghosh <iuippi3@gmail.com>
2020-05-18 11:21:56 +0530

    Lambdas in kotlin

--- diceRoll.kt
+++ diceRoll.kt
@@ -6,9 +6,9 @@ fun main (args:Array<String>)
     print("Enter sides in dice")
     val sides:Int = readLine()?.toIntOrNull() ?:0
 
-    val rollDice1 = { Random().nextInt(sides)+1 }
-    val rollDice2 = { sides:Int -> Random().nextInt(sides)+1}
-    val rollDice3 = { sides:Int -> if(sides==0)0 else Random().nextInt(sides)+1}
+    val rollDice1 = { Random().nextInt(sides)+1 } ()
+    val rollDice2 = { sides:Int -> Random().nextInt(sides)+1} (sides)
+    val rollDice3 = { sides:Int -> if(sides==0)0 else Random().nextInt(sides)+1} (sides)
 
     println(rollDice1)
     println(rollDice2)
